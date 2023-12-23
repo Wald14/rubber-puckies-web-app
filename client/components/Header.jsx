eadad6d92453007753f41a03b4ce98fd14aa3dae
Wald14 <lukewald@hotmail.com>
2023-12-22 21:15:48 -0600

    Started header / navbar. Added logo to assets/images folder

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -8,26 +8,48 @@ export default function Header() {
   const { user } = useAppCtx()
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">My Web Site</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            
-            { user?._id !== undefined && (
-              <Nav.Link href="/private">Private Page</Nav.Link>
-            )}
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src="/assets/images/rubber_puckie_logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            RUBBER PUCKIES
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
 
-            { user?._id !== undefined ? (
-              <Nav.Link href="/logout">Logout</Nav.Link>
-            ):(
-              <Nav.Link href="/auth">Login</Nav.Link>
-            )}
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+
+              <Nav.Link>Roster</Nav.Link>
+              <Nav.Link>Team History</Nav.Link>
+              <Nav.Link>Players</Nav.Link>
+              <Nav.Link>Season Log</Nav.Link>
+              <Nav.Link>Records</Nav.Link>
+
+              {user?._id !== undefined && (
+                <Nav.Link href="/private">Admin Controls</Nav.Link>
+              )}
+
+              {user?._id !== undefined ? (
+                <Nav.Link href="/logout">Logout</Nav.Link>
+              ) : (
+                <Nav.Link href="/auth">Login</Nav.Link>
+              )}
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
