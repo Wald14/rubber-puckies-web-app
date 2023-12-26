84e7068d76255a81936fc20f425004f72c6e68ac
Wald14 <lukewald@hotmail.com>
2023-12-26 13:29:29 -0600

    Adding two more photos to carousel

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -12,9 +12,21 @@ export default function UncontrolledExample() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="/assets/images/05_13_2023_rp_scrimmage_vs_mn+sentinels.jpg" style={{width: "100%"}}/>
+        <img src="/assets/images/05_13_2023_rp_scrimmage_vs_mn_sentinels.jpg" style={{width: "100%"}}/>
         <Carousel.Caption>
           <h3 style={{backgroundColor: "rgb(255, 255, 255, 0.5)"}}>May 2023 - Scrimmage vs Minnesota Sentinels</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="/assets/images/03_26_2023_winter_champions.JPEG" style={{width: "100%"}}/>
+        <Carousel.Caption>
+          <h3 style={{backgroundColor: "rgb(255, 255, 255, 0.5)"}}>Winter 2023 Champions</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="/assets/images/03_23_2022_rp_scrimmage_vs_mn_sentinels.JPG" style={{width: "100%"}}/>
+        <Carousel.Caption>
+          <h3 style={{backgroundColor: "rgb(255, 255, 255, 0.5)"}}>March 2022 - Scrimmage vs Minnesota Sentinels</h3>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
