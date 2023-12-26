858dfa3a52f4b7c3257d3cf21e68205ee9721f83
Wald14 <lukewald@hotmail.com>
2023-12-26 13:18:35 -0600

    Updated carousel to include new photo. Updated background of h3 tags in carousel so that they have an white background, 50% opacity, so it can still be read despite what is happening in the photo

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -8,23 +8,15 @@ export default function UncontrolledExample() {
       <Carousel.Item>
         <img src="/assets/images/championship_fall_2023.jpeg" style={{width: "100%"}}/>
         <Carousel.Caption>
-          <h3>Fall 2023 Champions</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* <Carousel.Item>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3 style={{backgroundColor: "rgb(255, 255, 255, 0.5)"}}>Fall 2023 Champions</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <img src="/assets/images/05_13_2023_rp_scrimmage_vs_mn+sentinels.jpg" style={{width: "100%"}}/>
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3 style={{backgroundColor: "rgb(255, 255, 255, 0.5)"}}>May 2023 - Scrimmage vs Minnesota Sentinels</h3>
         </Carousel.Caption>
-      </Carousel.Item> */}
+      </Carousel.Item>
     </Carousel>
     
   );
