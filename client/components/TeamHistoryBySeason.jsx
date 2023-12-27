107ccd02338a93af6bc6311f67cc82cd60d21b88
Wald14 <lukewald@hotmail.com>
2023-12-27 12:27:46 -0600

    Updated teamHistoryBySeason and teamHistory routes to now include playoff information. Models will need adjusting for new fields to fill in areas missing

--- client/components/TeamHistoryBySeason.jsx
+++ client/components/TeamHistoryBySeason.jsx
@@ -1,9 +1,9 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 
 import Table from 'react-bootstrap/Table';
 
 export default function TeamHistoryBySeason() {
-  const [teamArr, setTeamArr] = useState(null)  
+  const [teamArr, setTeamArr] = useState(null)
 
 
   async function getRegSeaInfo() {
@@ -25,8 +25,8 @@ export default function TeamHistoryBySeason() {
       <h3>Regular Season</h3>
       <Table responsive striped hover variant="dark">
         <thead>
-          <tr>
-            <th>Season</th>
+          <tr style={{ textAlign: "center" }}>
+            <th style={{ textAlign: "left" }}>Season</th>
             <th>GP</th>
             <th>Record</th>
             <th>Pts</th>
@@ -45,32 +45,74 @@ export default function TeamHistoryBySeason() {
           </tr>
         </thead>
         <tbody>
-          {teamArr && 
-          teamArr.map((season, key) => {
-            return (
-              <tr key={key}>
-                <td>{season.season}</td>
-                <td>{season.gamesPlayed}</td>
-                <td>{season.record}</td>
-                <td>{season.points}</td>
-                <td>{season.point_percentage}</td>
-                <td>{season.goals_for}</td>
-                <td>{season.goals_against}</td>
-                <td>{season.diff}</td>
-                <td>{season.goals_per_game}</td>
-                <td>{season.goals_against_per_game}</td>
-                <td>{season.diff_per_game}</td>
-                <td>{season.season_finish}</td>
-                <td>{season.playoff_finish}</td>
-                <td>{season.captain}</td>
-                <td>{season.champions}</td>
-                <td>{season.rink}</td>
-              </tr>
-            )
-          })}
+          {teamArr &&
+            teamArr.map((season, key) => {
+              return (
+                <tr key={key} style={{ textAlign: "center" }}>
+                  <td style={{ textAlign: "left" }}>{season.season}</td>
+                  <td>{season.regular.gamesPlayed}</td>
+                  <td>{season.regular.record}</td>
+                  <td>{season.regular.points}</td>
+                  <td>{season.regular.point_percentage}</td>
+                  <td>{season.regular.goals_for}</td>
+                  <td>{season.regular.goals_against}</td>
+                  <td>{season.regular.diff}</td>
+                  <td>{season.regular.goals_per_game}</td>
+                  <td>{season.regular.goals_against_per_game}</td>
+                  <td>{season.regular.diff_per_game}</td>
+                  <td>{season.regular.finish}</td>
+                  <td>{season.playoff.finish}</td>
+                  <td>{season.captain}</td>
+                  <td>{season.playoff.champion}</td>
+                  <td>{season.rink}</td>
+                </tr>
+              )
+            })}
+        </tbody>
+      </Table>
+
+
+      <h3>Playoffs</h3>
+      <Table responsive striped hover variant="dark">
+        <thead>
+          <tr style={{ textAlign: "center" }}>
+            <th style={{ textAlign: "left" }}>Season</th>
+            <th>Rds</th>
+            <th>GP</th>
+            <th>Record</th>
+            <th>SOW</th>
+            <th>SOL</th>
+            <th>GF</th>
+            <th>GA</th>
+            <th>Semi-Rd Opp</th>
+            <th>Champ Opp</th>
+            <th>Champ Score</th>
+            <th>Champions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teamArr &&
+            teamArr.map((season, key) => {
+              return (
+                <tr key={key} style={{ textAlign: "center" }}>
+                  <td style={{ textAlign: "left" }}>{season.season}</td>
+                  <td>{season.playoff.rounds}</td>
+                  <td>{season.playoff.gamesPlayed}</td>
+                  <td>{season.playoff.record}</td>
+                  <td>{season.playoff.sow}</td>
+                  <td>{season.playoff.sol}</td>
+                  <td>{season.playoff.gf}</td>
+                  <td>{season.playoff.ga}</td>
+                  <td>{season.playoff.semiRoundOpp}</td>
+                  <td>{season.playoff.championshipOpp}</td>
+                  <td>{season.playoff.championshipScore}</td>
+                  <td>{season.playoff.champion}</td>
+                </tr>
+              )
+            })}
         </tbody>
       </Table>
     </>
-    
+
   );
 }
--- server/routes/api/teamHistory.routes.js
+++ server/routes/api/teamHistory.routes.js
@@ -14,7 +14,24 @@ router.get("/:teamName", async (req, res) => {
 
     const payload = await Promise.all(
       teamSeasonInfo.map(async (season) => {
-        let wins = 0, loses = 0, ties = 0, goalsFor = 0, goalsAgainst = 0, gamesPlayed = 0
+        let wins = 0, 
+            loses = 0, 
+            ties = 0, 
+            goalsFor = 0, 
+            goalsAgainst = 0, 
+            gamesPlayed = 0
+
+        let playoffWins = 0, 
+            playoffLoses = 0, 
+            sow = 0, 
+            sol = 0,  
+            playoffGamesPlayed = 0, 
+            playoffGoalsFor = 0, 
+            playoffGoalsAgainst = 0,
+            champion = null,
+            championshipOpp = null,
+            championshipScore = null,
+            semiRoundOpp = null
 
         const seasonName = season.season.seasonType.charAt(0).toUpperCase() + season.season.seasonType.slice(1);
         const year = new Date(season.season.startDate).getFullYear();
@@ -23,8 +40,9 @@ router.get("/:teamName", async (req, res) => {
 
         seasonGameInfo.forEach((game) => {
           const completed = game.completed
+          const gameType = game.gameType
 
-          if (completed) {
+          if (completed && gameType === "regular") {
             gamesPlayed++
 
             const teamName = req.params.teamName;
@@ -44,25 +62,62 @@ router.get("/:teamName", async (req, res) => {
               ties++;
             }
           }
+
+          // PLAYOFF
+          if (completed && (gameType === "semi-finals" || gameType === "championship")) {
+            playoffGamesPlayed++
+
+            const teamName = req.params.teamName;
+            const isHomeTeam = game.homeTeam.name === teamName;
+            const isAwayTeam = game.awayTeam.name === teamName;
+            const homeGoals = game.homeGoals;
+            const awayGoals = game.awayGoals;
+
+            playoffGoalsFor += isHomeTeam ? homeGoals : awayGoals;
+            playoffGoalsAgainst += isHomeTeam ? awayGoals : homeGoals;
+
+
+            if ((isHomeTeam && homeGoals > awayGoals) || (isAwayTeam && awayGoals > homeGoals)) {
+              playoffWins++;
+            } else if ((isHomeTeam && homeGoals < awayGoals) || (isAwayTeam && awayGoals < homeGoals)) {
+              playoffLoses++;
+            }
+          }
         })
 
         return {
-          season: `${seasonName} ${year}`,
-          gamesPlayed: gamesPlayed,
-          record: `${wins} - ${loses} - ${ties}`,
-          points: wins * 2 + ties * 1,
-          point_percentage: ((wins * 2 + ties * 1) / (gamesPlayed * 2)).toFixed(3),
-          goals_for: goalsFor,
-          goals_against: goalsAgainst,
-          diff: goalsFor - goalsAgainst,
-          goals_per_game: (goalsFor / gamesPlayed).toFixed(1),
-          goals_against_per_game: (goalsAgainst / gamesPlayed).toFixed(1),
-          diff_per_game: ((goalsFor - goalsAgainst) / gamesPlayed).toFixed(1),
-          season_finsh: "",
-          playoff_finish: "",
           captain: season.captain.firstName + " " + season.captain.lastName,
-          champions: "",
           rink: "",
+          season: `${seasonName} ${year}`,
+
+          regular: {
+            diff: goalsFor - goalsAgainst,
+            diff_per_game: ((goalsFor - goalsAgainst) / gamesPlayed).toFixed(1),
+            finish: "",
+            gamesPlayed: gamesPlayed,
+            goals_against: goalsAgainst,
+            goals_for: goalsFor,
+            goals_against_per_game: (goalsAgainst / gamesPlayed).toFixed(1),
+            goals_per_game: (goalsFor / gamesPlayed).toFixed(1),
+            record: `${wins} - ${loses} - ${ties}`,
+            points: wins * 2 + ties * 1,
+            point_percentage: ((wins * 2 + ties * 1) / (gamesPlayed * 2)).toFixed(3),
+          },
+
+          playoff: {
+            champion: champion,
+            championshipOpp: championshipOpp,
+            championshipScore: championshipScore,
+            finish: "",
+            ga: playoffGoalsAgainst,
+            gamesPlayed: playoffGamesPlayed,
+            gf: playoffGoalsFor,
+            record: `${playoffWins} - ${playoffLoses}`,
+            rounds: "",
+            semiRoundOpp: semiRoundOpp,
+            sol: sol,
+            sow: sow,
+          }
         }
 
       })
