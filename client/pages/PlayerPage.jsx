b5883ab6c53ea80ac8c2aea1daeffb8d3b11d0d5
Wald14 <lukewald@hotmail.com>
2024-01-18 19:14:57 -0600

    Added sub-header bar on the PlayerPage and styled with CSS

--- client/pages/PlayerPage.jsx
+++ client/pages/PlayerPage.jsx
@@ -9,6 +9,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
 
+import '../assets/css/buttons.css'
 
 
 export default function PlayerPage() {
@@ -16,7 +17,9 @@ export default function PlayerPage() {
   const params = useParams()
 
   const [player, setPlayer] = useState()
+  const [selectedSplit, setSelectedSplit] = useState()
 
+  const handleChange = (e) => {setSelectedSplit(e.target.name)}
 
   async function getPlayerFromQuery() {
     const playerInfo = await getPlayer(params.playerid)
@@ -38,7 +41,14 @@ export default function PlayerPage() {
           <Col sm={12} md={6}>
             <Row><h2>{player.playerInfo.firstName} {player.playerInfo.lastName}</h2></Row>
             <Row>
-              <Col>#{player.playerInfo.jerseyNumber} {player.playerInfo.positions.join(", ")} {captializeString(player.playerInfo.handedness)}y</Col>
+              <Col>
+                <span style={{marginRight: "10px"}}>#{player.playerInfo.jerseyNumber}</span>
+                <span style={{marginRight: "10px"}}>{player.playerInfo.positions.join(", ")}</span>
+                <span>{captializeString(player.playerInfo.handedness)}y</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col>Started: {captializeString(player.statsBySeason[0].seasonStats.seasonInfo.seasonType)} {(new Date(player.statsBySeason[0].seasonStats.seasonInfo.startDate)).getFullYear()}</Col>
             </Row>
           </Col>
           <Col sm={12} md={6}>
@@ -66,6 +76,11 @@ export default function PlayerPage() {
               </tbody>
             </Table>
           </Col>
+        </Row>
+        <Row style={{borderBottom: "solid gold 1px", paddingBottom: "3px"}}>
+          <Col xs={3} md={1} ><a name="stats" className={`headerBtnA ${selectedSplit === "stats" ? 'selected' : ''}`} onClick={handleChange} >Stats</a></Col>
+          <Col xs={3} md={1} ><a name="splits" className={`headerBtnA ${selectedSplit === "splits" ? 'selected' : ''}`} onClick={handleChange} >Splits</a></Col>
+          <Col xs={6} md={2} ><a name="gamelog" className={`headerBtnA ${selectedSplit === "gamelog" ? 'selected' : ''}`} onClick={handleChange} >Game Log</a></Col>
         </Row>
       </Container>
     </>
