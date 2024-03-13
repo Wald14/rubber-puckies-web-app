9af091c74b024419c4762754f40c149ecac02e64
Wald14 <lukewald@hotmail.com>
2024-03-13 16:17:39 -0500

    Updated season log to now have an optional path where the selected season of interest can be passed in the url and then that season's accord will auto open on page loading

--- client/components/SeasonAccord.jsx
+++ client/components/SeasonAccord.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
 import { GamesAccord } from '../components';
 import Accordion from 'react-bootstrap/Accordion';
 import Table from 'react-bootstrap/Table';
@@ -8,6 +9,8 @@ import '../assets/css/accordion.css'
 
 export default function SeasonAccord() {
   const [seasonArr, setSeasonArr] = useState(null)
+  const [activeSeason, setActiveSeason] = useState(null);
+  const { seasonname } = useParams()
 
   async function getSeasonLogs() {
     const query = await fetch("/api/team/name/Rubber Puckies");
@@ -20,21 +23,37 @@ export default function SeasonAccord() {
     console.log(result)
   }
 
+  const handleAccordionClick = (index) => {
+    setActiveSeason(activeSeason === index ? null : index);
+  };
+
+
   useEffect(() => {
     getSeasonLogs()
   }, [])
+
+  useEffect(() => {
+    if (seasonArr) {
+      const index = seasonArr.findIndex(season => (season.season.seasonType.toLowerCase() +  new Date(season.season.startDate).getFullYear()) === seasonname)
+      if (index !== -1) {
+        setActiveSeason(String(index))
+      }
+      console.log("Index:", index, "seasonId:", seasonname)
+    }
+  }, [seasonArr, seasonname])
 
   if (!seasonArr) return <></>
 
   return (
     <>
-      <Accordion id="seasonAccord">
+      <Accordion id="seasonAccord" activeKey={activeSeason}>
         {seasonArr &&
           seasonArr.map((season, key) => {
             return (
               <Accordion.Item
-                id={`${capitalizeString(season.season.seasonType).toLowerCase()}${new Date(season.season.startDate).getFullYear()}`}
-                eventKey={key}
+                id={`${season.season.seasonType.toLowerCase()}${new Date(season.season.startDate).getFullYear()}`}
+                onClick={() => handleAccordionClick(String(key))}
+                eventKey={String(key)}
                 key={key}
                 style={{
                   backgroundColor: "#141414",
