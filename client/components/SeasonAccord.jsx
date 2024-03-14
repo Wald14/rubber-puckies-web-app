907ba6abc937c4bb51f33a4a3f7f378ac96e173e
Wald14 <lukewald@hotmail.com>
2024-03-14 00:01:23 -0500

    Another attempt at fixing bug

--- client/components/SeasonAccord.jsx
+++ client/components/SeasonAccord.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { GamesAccord } from '../components';
 import Accordion from 'react-bootstrap/Accordion';
 import Table from 'react-bootstrap/Table';
@@ -9,6 +9,8 @@ import '../assets/css/accordion.css'
 export default function SeasonAccord() {
   const [seasonArr, setSeasonArr] = useState(null)
   const [activeSeason, setActiveSeason] = useState(null);
+  const [accordionTransitionEnded, setAccordionTransitionEnded] = useState(false);
+  const accordionBodyRef = useRef(null);
 
   async function getSeasonLogs() {
     const query = await fetch("/api/team/name/Rubber Puckies");
@@ -24,7 +26,7 @@ export default function SeasonAccord() {
     setActiveSeason(activeSeason === index ? null : index);
   };
 
-  const scrollToHash = async () => {
+  const scrollToHash = () => {
     if (seasonArr) {
       const index = seasonArr.findIndex(season =>
         (season.season.seasonType.toLowerCase() + new Date(season.season.startDate).getFullYear()) === (window.location.hash.substring(1)))
@@ -36,9 +38,9 @@ export default function SeasonAccord() {
         const hash = window.location.hash.substring(1)
         const element = document.getElementById(hash)
         if (element) {
-          setTimeout(() => {
+          // setTimeout(() => {
             element.scrollIntoView({ behavior: 'smooth', block: 'start' })
-          }, 1000)
+          // }, 1000)
         }
       }
     }
@@ -48,9 +50,31 @@ export default function SeasonAccord() {
     getSeasonLogs()
   }, [])
 
+  // useEffect(() => {
+  //   scrollToHash()
+  // }, [seasonArr])
+
   useEffect(() => {
-    scrollToHash()
-  }, [seasonArr])
+    setTimeout(() => {
+    if (!accordionBodyRef.current?.hidden) {
+      console.log(accordionBodyRef)
+      setAccordionTransitionEnded(true)
+      console.log("here")
+      scrollToHash()
+    }
+  }, 500)
+  }, [accordionBodyRef.current])
+
+  useEffect(() => {
+    // console.log(accordionBodyRef.current)
+    // setAccordionTransitionEnded(true)
+    if (accordionTransitionEnded) {
+      console.log("here2")
+      scrollToHash()
+    }
+  }, [accordionTransitionEnded])
+
+
 
   // useEffect(() => {
   //   if (seasonArr) {
@@ -82,7 +106,7 @@ export default function SeasonAccord() {
                 }}
               >
                 <Accordion.Header>{capitalizeString(season.season.seasonType)} {new Date(season.season.startDate).getFullYear()} - {season.season.rink}</Accordion.Header>
-                <Accordion.Body>
+                <Accordion.Body  ref={accordionBodyRef}>
 
                   <GamesAccord teamId={season.season._id} playoffPlace={season.playoffPlace} />
 
